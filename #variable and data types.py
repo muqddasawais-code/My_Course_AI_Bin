b801ae5309f750c26e81f9b2fe321428088437af
muqddasawais-code <muqddasawais@gmail.com>
2026-03-05 12:01:52 -0800

    assignment complete /week1 by muqddas

---
+++ #variable and data types.py
@@ -0,0 +1,57 @@
+#variable and data types:
+#Create an integer variable age and a float variable height. Print their types.
+age=24
+height=3.22
+print(type(age))
+print(type(height))
+      
+
+
+#12. store the value 3 + 4j in a variable. Print the variable and its type.
+a=3+3j
+print(type(a))
+print(a)
+
+
+
+#Create a boolean variable is_python_fun and set it to True.
+is_python_fun=True
+print(is_python_fun)
+
+
+
+#: Assign three different values o three variables in a single line
+a=4;b=3;c=6
+print(a,b,c)
+
+
+
+#15.  Assign the same value to three different variables in a single line.
+a=b=c=5
+print(a,b,c)
+
+
+#16. Take a numeric input from a user and convert it to a float.
+num=float(input("enter a number:"))
+print(type(num))
+
+
+#17. Take a string input "100" and convert it to an int.
+a=int(input("100"))
+print(type(a))
+print(a)
+
+#Create a variable with a complex number and print only its real part.
+a=3+3j
+print(a.real)
+
+
+#19. Define a string variable containing a paragraph and print its length.
+paragraph="This is a sample paragraph to test the length of the string variable.6"
+print(len(paragraph))
+
+#20. Swap the values of two variables a and b without using a third variable.
+#a=4
+#b=2
+#print(a,b = b,a)
+
